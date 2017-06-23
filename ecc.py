e2558f23bc910b54860fe071106192aa0908e61e
Shivam Arora <shivamarr96@gmail.com>
2017-06-23 18:57:57 +0530

    Needs more optimisation
    Removed print statements and then included the timer for encryption and decryption.

--- ecc.py
+++ ecc.py
@@ -32,24 +32,22 @@ dec_times_data = []
 tot_times_data = []
 while 1:
     dataBuffer = ''
-    t1 = time.time()
-    tot1 = t1
+    
+    
     packet = []
     if dataSize > N: #This chosen value signifies the number of clouds we will divide the data on. If data is short of this value we will fill it with dummy values like spaces. -Anshuman
         dataBuffer = byteString[N:]
         byteString = byteString[:N]
-
-    print dataSize
-    print len(byteString)
-
-    for x in range(7):
-        packet.append(byteString[(x*32):((32*(x+1)))])
-        print packet[x]
-    # Step 2
-
     enc_list = ['001','010','011', '100', '101', '110', '111']
     r = []
-    
+    #print dataSize
+    #print len(byteString)
+    t1 = time.time()
+    tot1 = t1
+    for x in range(7):
+        packet.append(byteString[(x*32):((32*(x+1)))])
+        #print packet[x]
+    # Step 2
     for y in range(7):
         r.append(random.choice(enc_list))
         enc_list.remove(r[y])
@@ -57,8 +55,8 @@ while 1:
         #print packet[y]
     
     #private_key = '101010101101111100010110011101010'
-    private_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(32))
-    print private_key
+    private_key = random.choice(string.digits)
+    #print private_key
     public_key = str(seccure.passphrase_to_pubkey(private_key))
     ciphertext = []
     decrypted = []
@@ -66,32 +64,9 @@ while 1:
     ###Encryption phase
     for z in range(7):
         ciphertext.append(seccure.encrypt(packet[z][:-3], public_key))
-        print ciphertext[z]
-
-        if packet[z][-3:] == '000':
-            print 'SEND packet[z][:-3] TO CLOUD 1'
-
-        if packet[z][-3:] == '001':
-            print 'SEND packet[z][:-3] TO CLOUD 2'
-
-        if packet[z][-3:] == '010':
-            print 'SEND packet[z][:-3] TO CLOUD 3'
-
-        if packet[z][-3:] == '011':
-            print 'SEND packet[z][:-3] TO CLOUD 4'
-
-        if packet[z][-3:] == '100':
-            print 'SEND packet[z][:-3] TO CLOUD 5'
-            
-        if packet[z][-3:] == '101':
-            print 'SEND packet[z][:-3] TO CLOUD 6'
-
-        if packet[z][-3:] == '110':
-            print 'SEND packet[z][:-3] TO CLOUD 7'
-
-        if packet[z][-3:] == '111':
-            print 'SEND packet[z][:-3] TO CLOUD 8'
-
+        #print ciphertext[z]
+    t2 = time.time()
+        
     ###Decryption phase
     #For benchmarking and an easier to understand algorithm we are not fetching data from the different cloud sources and combining it as our purpose here is to test the speed of this proposed encryption algorithm against RSA
     #Ideally, this would go something like this -
@@ -104,25 +79,25 @@ while 1:
     #RECEIVE packet data from CLOUD 7
     #RECEIVE packet data from CLOUD 8
     #Combine packets and store in the list cyphertext in the order that they appeared
-    t2 = time.time()
+    
     print 'Encryption Time for the iteration',t2-t1
     enc_times_data.append(t2-t1)
-    t1 = time.time()
+    
     decrypted_bytestring = ''
-
+    t1 = time.time()
     for k in range(7):
         decrypted.append(seccure.decrypt(ciphertext[k], private_key))
-        print decrypted[k]
+        #print decrypted[k]
         decrypted_bytestring += decrypted[k]
 
-    print decrypted_bytestring == byteString
-    print len(decrypted_bytestring)
+    #print decrypted_bytestring == byteString
+    #print len(decrypted_bytestring)
 
     strdec = ''
     for l in range(len(decrypted_bytestring)/7):
         strdec += chr(int(decrypted_bytestring[l*7:(l+1)*7],2))
     
-    print strdec
+    #print strdec
     decryptedTextString += strdec
 
     t2 = time.time()
@@ -131,7 +106,7 @@ while 1:
     dec_times_data.append(t2-t1)
     print 'Total Time for the iteration',tot2-tot1
     tot_times_data.append(tot2-tot1)
-    
+
     byteString = dataBuffer
     if len(dataBuffer) == 0:
         break
